1c740013865198fb4270656be0d846cae8c2316c
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-05-17 13:41:14 -0600

    tic-tac toe

--- Source.cpp
+++ Source.cpp
@@ -1,197 +1,81 @@
+
 #include <iostream>
-#include <vector>
-#include<algorithm>
-#include <fstream>
-#include <string>
 using namespace std;
+char grid[5][5];
+ 
+int input;
+int input2;
 
 int main() {
-	string A = "Alpha";
-	string B = "Bravo";
-	string C = "Charlie";
-	string D = "Delta";
-	string E = "Echo";
-	string F = "Foxtrot";
-	string G = "Golf";
-	string H = "Hotel";
-	string I = "India";
-	string J = "Juliet";
-	string K = "Kilo";
-	string L = "Lima";
-	string M = "Mike";
-	string N = "November";
-	string O = "Oscar";
-	string P = "Papa";
-	string Q = "Quebec";
-	string R = "Romeo";
-	string S = "Sierra";
-	string T = "Tango";
-	string U = "Uniform";
-	string V = "Victor";
-	string W = "Whiskey";
-	string X = "X-ray";
-	string Y = "Yankee";
-	string Z = "Zulu";
 
-	//1. set up vector
-	char mili;
-	vector<char> mili2;
-	vector<char>::iterator iter1;
-	vector<string>::iterator iter2;
-	vector<string>mili3;
-	//2. get input from user
+	char grid[5][5] = { ' ','|',' ','|', ' ' ,
+						'-','-','-','-','-',
+						' ', '|',' ','|', ' ',
+						'-', '-','-', '-', '-',
+						' ', '|',' ', '|', ' ', };
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << grid[i][j];
+		}
+		cout << " " << endl;;
+	}
+	char input;
+	int input1;
+	int input2;
+	while (1) {
+		cout << "enter X or O" << endl;
+		cin >> input;
+		cout << "what row" << endl;
+		cin >> input1;
+		cout << "what collumn" << endl;
+		cin >> input2;
+		
+		
+		if (input1 == 1)input1 = 0;
+		if (input1 == 2)input1 = 2;
+		if (input1 == 3)input1 = 4;
 
+		if (input2 == 1)input2 = 0;
+		if (input2 == 2)input2 = 2;
+		if (input2 == 3)input2 = 4;
 
+		if (input == 'X' || input == 'x') {
+			grid[input1][input2] = 'X';
+		}
 
-	ifstream infile; //gets ready to open file
-	infile.open("sos.txt"); //open the file
+		bool placed = false;
+		while(1) {
+			int a = rand() % 5;
+			int b = rand() % 5;
+			if (grid[a][b] == ' ') {
+				grid[a][b] = 'O';
+				break;
+			}
 
-	int num;//we added a number at the top of the file to make this easier, this will hold it
-	//read in the first number
-	infile >> num; //read in the first number, use it to run the for loop!
+		}
+		if (input1 == 1)input1 = 0;
+		if (input1 == 2)input1 = 2;
+		if (input1 == 3)input1 = 4;
 
-	//reads in the rest of the characters from file
-	for (int i = 0; i < num; i++) {
-		infile >> mili; //temp storage
-		mili2.push_back(mili); //push into the vector
+		if (input2 == 1)input2 = 0;
+		if (input2 == 2)input2 = 2;
+		if (input2 == 3)input2 = 4;
+
+		if (input == 'O' || input == 'o') {
+			grid[input1][input2] = 'O';
+
+		}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				cout << grid[i][j];
+			}
+			cout << " " << endl;
 	}
 
-	//walks through the character vector
-	for (iter1 = mili2.begin(); iter1 != mili2.end(); iter1++) {
-		if (*iter1 == 'a' || *iter1 == 'A') {
-			mili3.push_back(A); //push "alpha" into the string vector
-		}
-		//25 more if statements here
-
-
-
-		if (*iter1 == 'b' || *iter1 == 'B') {
-			mili3.push_back(B); //push "alpha" into the string vector
-
-
-		}
-
-		if (*iter1 == 'c' || *iter1 == 'C') {
-			mili3.push_back(C); //push "alpha" into the string vector
-
-		//25 more if statements here
-
-
-			if (*iter1 == 'D' || *iter1 == 'D') {
-				mili3.push_back(D); //push "alpha" into the string vector
-
-			//25 more if statements here
-			}
-
-			if (*iter1 == 'e' || *iter1 == 'E') {
-				mili3.push_back(E); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'f' || *iter1 == 'F') {
-				mili3.push_back(F); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-			if (*iter1 == 'g' || *iter1 == 'G') {
-				mili3.push_back(G); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'h' || *iter1 == 'H') {
-				mili3.push_back(H); //push "alpha" into the string vector
-			}
-
-
-			if (*iter1 == 'i' || *iter1 == 'I') {
-				mili3.push_back(I); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'j' || *iter1 == 'J') {
-				mili3.push_back(J); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'k' || *iter1 == 'K') {
-				mili3.push_back(K); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'l' || *iter1 == 'L') {
-				mili3.push_back(L); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'm' || *iter1 == 'M') {
-				mili3.push_back(M); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'n' || *iter1 == 'N') {
-				mili3.push_back(N); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'o' || *iter1 == 'O') {
-				mili3.push_back(O); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'p' || *iter1 == 'P') {
-				mili3.push_back(P); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-			if (*iter1 == 'q' || *iter1 == 'Q') {
-				mili3.push_back(Q); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-			if (*iter1 == 'r' || *iter1 == 'R') {
-				mili3.push_back(S); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-
-			if (*iter1 == 's' || *iter1 == 'S') {
-				mili3.push_back(S); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-
-			if (*iter1 == 't' || *iter1 == 'T') {
-				mili3.push_back(T); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-			if (*iter1 == 'u' || *iter1 == 'U') {
-				mili3.push_back(U); //push "alpha" into the string vector
-			}
-			//25 more if statements here
-
-			if (*iter1 == 'v' || *iter1 == 'V') {
-				mili3.push_back(V); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'w' || *iter1 == 'W') {
-				mili3.push_back(W); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'x' || *iter1 == 'X') {
-				mili3.push_back(X); //push "alpha" into the string vector
-			}
-
-			if (*iter1 == 'y' || *iter1 == 'Y') {
-				mili3.push_back(Z); //push "alpha" into the string vector
-
-				if (*iter1 == 'z' || *iter1 == 'Z') {
-					mili3.push_back(Z); //push "alpha" into the string vector
-				}
-				//25 more if statements here
-
-			}
-		}
-		//4. print it
-
-		ofstream write;
-		write.open("changedSOS.txt");
-		for (iter2 = mili3.begin(); iter2 != mili3.end(); iter2++) {//walk through the string array
-			write << *iter2 << endl; //write each slot to the text file
-		}
-		write.close();
-
 	}
-	return 1;
 }
+
+	
+
+	
